244c60d18c3e203ad36c6b7f2b7606300301fbeb
Devin Gray <admin@devingray.io>
2023-09-05 21:02:49 -0700

    added api call functionality with react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { Suspense } from "react";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from 'react-query'
+const NewsFeed = React.lazy(() => import("./NewsFeed"));
 import './App.css'
+import './custom.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      suspense: true,
+    },
+  },
+})
+
+function App () {
+  const title = "LinkedIn News";
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <h1>{title}</h1>
+          <Suspense fallback={<div>Loading...</div>}>
+            <NewsFeed />
+          </Suspense>
+        </div>
+      </QueryClientProvider>
     </>
   )
 }
---
+++ src/NewsFeed.tsx
@@ -0,0 +1,58 @@
+import { useEffect, useState } from 'react';
+import {
+  useQuery,
+} from 'react-query'
+
+type NewsItem = {
+  headline: string;
+  date: string;
+  readers: string;
+};
+export default function NewsFeed () {
+  const [newsData, setNewsData] = useState<NewsItem[]>([]);
+  const [showAll, setShowAll] = useState(false);
+
+  function toggleShowAll () {
+    setShowAll(prev => !prev);
+  }
+
+  async function fetchNews () {
+    const response = await fetch(`/news.json`);
+    if (!response.ok) throw new Error('Network response was not ok')
+    return response.json()
+  }
+
+  const { isLoading, isError, error, data } = useQuery({ queryKey: ['newsData'], queryFn: fetchNews })
+
+  useEffect(() => {
+    if (showAll) {
+      setNewsData(data.data);
+    } else {
+      setNewsData(data.data.slice(0, 5));
+    }
+  }, [showAll]);
+
+  if (isLoading) {
+    return <span>Fetching data...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error instanceof Error && error.message}</span>
+  }
+
+  return (
+    <>
+      <ul className='newsfeed'>
+        {newsData.map((newsItem: NewsItem) => {
+          return (
+            <li>
+              <p><strong>{newsItem.headline}</strong></p>
+              <span>{newsItem.date} {newsItem.readers}</span>
+            </li>
+          );
+        })}
+      </ul>
+      <button onClick={toggleShowAll}>Show {showAll ? 'Less' : 'More'}</button>
+    </>
+  );
+}
